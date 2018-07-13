e89e72cc42113cac23f71f90d0d434d060080ff8
JessyDominguez <dominguezjesica1989@gmail.com>
2018-07-13 15:20:24 -0500

    se sube funcionalidad

--- js/src/app.js
+++ js/src/app.js
@@ -11,3 +11,24 @@ consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
 culpa qui officia deserunt mollit anim id est laborum.`;
+
+
+const gettingLinksFromMarkdown = (md) => {
+    const rgx = /(\[(.*?)\])|(https?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi;
+    let matchRgxAndMd = md.match(rgx);//Retorna array con texto y href 
+
+    console.log(matchRgxAndMd);
+
+    let hrefAndTextMd = [];
+
+    for(var i = 0; i < matchRgxAndMd.length; i++) {
+        hrefAndTextMd.push({ //Se ingresa al array cada objeto con las propiedades href y text
+            href: matchRgxAndMd[i + 1],
+            text: matchRgxAndMd[i].slice(1,-1)
+        });
+        i++;
+    }
+    console.log(hrefAndTextMd); //Devuelve el array de objetos (links)
+}
+
+gettingLinksFromMarkdown(str);
