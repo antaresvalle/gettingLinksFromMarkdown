7e5ebd0f36246ab7097b7f23d80aa93036478d31
antaresvalle <antaresvll561@gmail.com>
2018-07-20 10:16:46 -0500

    se actualiza archivo package.json y se agrega la función principal a una función anónima autoejecutada

--- src/gettingLinksFromMarkdown.js
+++ src/gettingLinksFromMarkdown.js
@@ -1,25 +1,30 @@
-const gettingLinksFromMarkdown = (md) => {
-    let mdWithoutImg = String(md).replace(/!\[([^\[.]+?)\]\((.+?)\)/gm, '');
-    //console.log(mdWithoutImg);
+(function(){
 
-    const rgx = /!?\[([^\[.]+?)\]\((.+?)\)/gm //arroja todos los links 
-
-    let hrefAndTextMd = [];
-
-    let result = '';
-    while (result = rgx.exec(mdWithoutImg)) {
-        hrefAndTextMd.push({ //Se ingresa al array cada objeto con las propiedades href y text
-            href: result[2],
-            text: result[1]
-        });
+    const gettingLinksFromMarkdown = (md) => {
+        let mdWithoutImg = String(md).replace(/!\[([^\[.]+?)\]\((.+?)\)/gm, '');
+        //console.log(mdWithoutImg);
+    
+        const rgx = /!?\[([^\[.]+?)\]\((.+?)\)/gm //arroja todos los links 
+    
+        let hrefAndTextMd = [];
+    
+        let result = '';
+        while (result = rgx.exec(mdWithoutImg)) {
+            hrefAndTextMd.push({ //Se ingresa al array cada objeto con las propiedades href y text
+                href: result[2],
+                text: result[1]
+            });
+        }
+        
+        return hrefAndTextMd;
     }
     
-    return hrefAndTextMd;
-}
+    //exportando globalmente
+    if (typeof window == 'undefined') {
+        module.exports = gettingLinksFromMarkdown;
+    } else {
+        window.gettingLinksFromMarkdown = gettingLinksFromMarkdown;
+    }
+    
+})();
 
-//exportando globalmente
-if (typeof window == 'undefined') {
-    module.exports = gettingLinksFromMarkdown;
-} else {
-    window.gettingLinksFromMarkdown = gettingLinksFromMarkdown;
-}
